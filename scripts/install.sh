7469f96b45b1c8778e37ebe513d4691abcf07ceb
btopro <bto108@psu.edu>
2016-02-10 13:37:48 -0500

    support for cent6

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,18 @@
 # tokenized values so this can also be setup with 1 line
 # since we'll be spinning up multiple boxes
 if [[ $1 = 'ubuntu' ]]; then
-  yes | apt-get -y install git wget && yes | apt-get update
+  yes | apt-get -y install git wget
+  yes | apt-get update
+elif [[ $1 = 'centos' ]]; then
+  yes | yum -y install git wget
+  yes | yum update
+  wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+  rpm -Uvh remi-release-6*.rpm
+  wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+  rpm -Uvh epel-release-6*.rpm
 else
-  yes | yum -y install git wget && yes | yum update
+  yes | yum -y install git wget
+  yes | yum update
 fi
 git clone https://github.com/elmsln/elmsln.git /var/www/elmsln && bash /var/www/elmsln/scripts/install/handsfree/$1/$1-install.sh $2 $3 $4 $5 admin@elmsln.dev no
 
